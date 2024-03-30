89b29b471153158bc9311cc1ac4a88dfa250e2df
Umesh <umeshsarkar19@outlook.com>
2024-03-30 16:35:46 +0530

    Adding database

--- app/database/user.py
+++ app/database/user.py
@@ -1,2 +1,15 @@
-class User:
-    pass
+from shared import db
+
+
+class User(db.Model):
+    user_id = db.Column("uid",db.Integer, primary_key = True)
+    username = db.Column("name",db.String(50), unique = True, nullable = False)
+    email = db.Column("email",db.String(50), nullable= False)
+    password = db.Column("pass",db.String(50), nullable = False)
+    
+    def __init__(self, uid, name, email, password):
+         self.user_id = uid
+         self.username = name
+         self.email = email
+         self.password = password
+        
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from app.database.shared import db
+from app.route import route
 
 
 ## for testing purposes
@@ -8,18 +10,59 @@ from summarizers import extract_sum
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db.init_app(app)
+
 
 @app.route("/")
-def mainPage():
-    
+def main():
     return render_template('index.html')
+
+@app.route('/login', methods=["POST","GET"])
+def login():
+    
+    if request.method == "GET":
+        return render_template('log.html')
+    
+    elif request.method == "POST":
+        session = "login"
+
+        if session == "signup": 
+            name = request.args.get("uname_signup")    
+            email = request.args.get("email_signup")
+            password = request.args.get("pass_signup")
+        
+        elif session == "login":
+   
+            name = request.args.get("uname_login")
+            password = request.args.get("pass_login")
+            
+            
+            
+        
+        
+    
+        
+        
+
+
+# @app.route('/home')
+# def home():
+#     return
 
 
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
     
+    
+    with app.app_context():
+        db.create_all()
+        app.run(debug=True)
+        
+    # pdf = PDFHandler.PDFReader("text_files/halo.pdf")
+    # text = pdf.text()
+    # print(extract_sum.summarize(text,0.5))
     
     
    
