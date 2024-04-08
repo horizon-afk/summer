4ded609159fd36ea542f1dade73510f3f3936835
Umesh <umeshsarkar19@outlook.com>
2024-04-08 11:40:05 +0530

    Connected backend to frontend

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from app.database.shared import db
 from app.route import route
+from werkzeug.utils import secure_filename
+import os
 
 
 ## for testing purposes
@@ -9,14 +11,50 @@ import app.manager.PDFHandler as PDFHandler
 from summarizers import extract_sum
 
 
+UPLOAD_FOLDER = "uploads"
+ALLOWED_EXTENSIONS = {'pdf'}
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 db.init_app(app)
 
 
-@app.route("/")
+@app.route("/", methods=["POST","GET"])
 def main():
-    return render_template('index.html')
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(os.path.join(app.config["UPLOAD_FOLDER"], secure_filename(f.filename)))
+    
+        return 'file uploaded'
+        
+    if request.method == 'GET':
+        return render_template('index.html')
+    
+    
+@app.route("/home", methods=["POST","GET"])
+def home():
+    
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(os.path.join(app.config["UPLOAD_FOLDER"], secure_filename(f.filename)))
+        
+        path_to_file = "uploads/"+ f.filename
+        pdf = PDFHandler.PDFReader(path_to_file)
+        text = pdf.text()
+        os.remove(path_to_file)
+        
+        summary = extract_sum.summarize(text,0.5)
+        
+        
+        return render_template('home.html', summary = summary)
+    
+    
+    if request.method == 'GET':
+        summary= ""
+        ## clear the upload direectory
+        return render_template('home.html')
+    
 
 @app.route('/login', methods=["POST","GET"])
 def login():
@@ -37,12 +75,13 @@ def login():
             name = request.args.get("uname_login")
             password = request.args.get("pass_login")
             
-            
-            
-        
-        
-    
-        
+
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS          
+
         
 
 
@@ -60,9 +99,7 @@ if __name__ == "__main__":
         db.create_all()
         app.run(debug=True)
         
-    # pdf = PDFHandler.PDFReader("text_files/halo.pdf")
-    # text = pdf.text()
-    # print(extract_sum.summarize(text,0.5))
+    
     
     
    
