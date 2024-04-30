01265f7fb0f719cdc38c1946200775a8872f8f8d
Umesh <umeshsarkar19@outlook.com>
2024-04-30 11:58:25 +0530

    Cleaned the code space

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 from flask import Flask, render_template, request, redirect
-from flask_sqlalchemy import SQLAlchemy
-from app.database.shared import db
-from app.route import route
 from werkzeug.utils import secure_filename
 import os
 
 
-## for testing purposes
 import app.manager.PDFHandler as PDFHandler
-from summarizers import extract_sum, abstrat
+from summarizers import abstrat
 
 
 UPLOAD_FOLDER = "uploads"
@@ -17,87 +13,43 @@ ALLOWED_EXTENSIONS = {'pdf'}
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-db.init_app(app)
 
 
+    
 @app.route("/", methods=["POST","GET"])
-def main():
-    if request.method == 'POST':
-        f = request.files['file']
-        f.save(os.path.join(app.config["UPLOAD_FOLDER"], secure_filename(f.filename)))
-    
-        return 'file uploaded'
-        
-    if request.method == 'GET':
-        return render_template('index.html')
-    
-    
-@app.route("/home", methods=["POST","GET"])
 def home():
+    text = ""
+    summary = ""
     
+    #when a POST request is made to the website
     if request.method == 'POST':
-        f = request.files['file']
-        f.save(os.path.join(app.config["UPLOAD_FOLDER"], secure_filename(f.filename)))
         
-        path_to_file = "uploads/"+ f.filename
-        pdf = PDFHandler.PDFReader(path_to_file)
-        text = pdf.text()
-        os.remove(path_to_file)
+        #when user wants to upload a PDF
+        if request.form['action'] == "Upload":
+            f = request.files['file']
+            secure_file = secure_filename(f.filename)
+            f.save(os.path.join(app.config["UPLOAD_FOLDER"], secure_file))
         
+            path_to_file = "uploads/"+ secure_file
+            pdf = PDFHandler.PDFReader(path_to_file) #extracts text from a custom made PDF parser
+            text = pdf.text()
+            os.remove(path_to_file)
+            
+        #when user wants the text to be processed  
+        elif request.form['action'] == "Summary":
+            text = request.form.get('text')  
+        
+        #summarizes the text based on the data fed
         summary = abstrat.summarize(text)
         
-        
-        return render_template('home.html', summary = summary)
-    
-    
+    #when a GET request is made to the website on startup
     if request.method == 'GET':
         summary= ""
-        ## clear the upload direectory
-        return render_template('home.html')
-    
-
-@app.route('/login', methods=["POST","GET"])
-def login():
-    
-    if request.method == "GET":
-        return render_template('log.html')
-    
-    elif request.method == "POST":
-        session = "login"
-
-        if session == "signup": 
-            name = request.args.get("uname_signup")    
-            email = request.args.get("email_signup")
-            password = request.args.get("pass_signup")
         
-        elif session == "login":
-   
-            name = request.args.get("uname_login")
-            password = request.args.get("pass_login")
-            
-
-
-
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS          
-
-        
-
-
-
-
-
+    return render_template('home.html', summary = summary)
+    
+#main entry point of the app
 if __name__ == "__main__":
     
-    
-    with app.app_context():
-        db.create_all()
-        app.run(debug=True)
-        
-    
-    
-    
-   
-    
-    pass
+    with app.app_context():      
+        app.run()
--- summarizers/abstrat.py
+++ summarizers/abstrat.py
@@ -9,7 +9,7 @@ def summarize(text: str):
 
     tokens_input = tokenizer.encode("summarize: " + text,
                               return_tensors='pt',
-                              max_length=tokenizer.model_max_length,
+                              max_length=512,
                               truncation=True)
 
     summary_ids = model.generate(tokens_input, min_length=80, 
@@ -19,25 +19,18 @@ def summarize(text: str):
     return summary
 # Example usage:
 input_text = """
-World War II, was a colossal struggle that spanned from 1939 to 1945, engulfed most of the globe.
-The main combatants were divided into two alliances: the Allies and the Axis. The Allies, initially led
-by Great Britain and France, eventually included the United States and the Soviet Union.Axis powers,
-spearheaded by Nazi Germany, Fascist Italy, and Imperial Japan.The war's roots can be traced back to
-unresolved grievances from World War I, particularly the harsh Treaty of Versailles imposed on
-Germany. This, coupled with the rise of totalitarian dictators like Adolf Hitler in Germany, Benito
-Mussolini in Italy, and militaristic expansionism in Japan, created a volatile atmosphere. The invasion
-of Poland by Germany in 1939 ignited the war in Europe, with Britain and France declaring war in
-response.The initial years saw Germany conquer much of Europe through blitzkrieg tactics. The tide
-began to turn with the German invasion of the Soviet Union in 1941 and the Japanese attack on Pearl
-Harbor in 1941, drawing the US into the war. The conflict then became a brutal fight on multiple
-fronts, from the icy plains of Eastern Europe to the scorching deserts of North Africa, and the vast
-Pacific Ocean.The war wasn't just fought on battlefields. Technological advancements led to the
-development of new weapons like jet fighters, rockets, and the atomic bomb, dropped by the US on
-Hiroshima and Nagasaki in Japan in 1945.Civilian populations also became targets, with horrific
-atrocities committed by the Axis powers, particularly the Holocaust, the systematic extermination of
-Jews by Nazi Germany.After immense sacrifice, the Allies emerged victorious in 1945. The war's
-consequences were devastating. Millions lay dead, countless cities were destroyed, and the global
-order was forever altered.
+New York (CNN)When Liana Barrientos was 23 years old, she got married in Westchester County, NewYork.A year later, she got married again in Westchester County, but to a different man and without
+divorcing her first husband.Only 18 days after that marriage, she got hitched yet again. Then, Barrientos declared "I do" five more times, sometimes only within two weeks of each other.In 2010, she married once more, this time in the Bronx. In an application for a marriage license, she stated it
+was her "first and only" marriage.Barrientos, now 39, is facing two criminal counts of "offering a falseinstrument for filing in the first degree," referring to her false statements on the
+2010 marriage license application, according to court documents.Prosecutors said the marriages werepart of an immigration scam.On Friday, she pleaded not guilty at State Supreme Court in the Bronx, according to her attorney, Christopher Wright, who declined to comment further.After leaving court, Barrientos was arrested and charged with theft of service and criminal trespass for allegedly sneaking
+into the New York subway through an emergency exit, said Detective Annette Markowski, a police
+spokeswoman. In total, Barrientos has been married 10 times, with nine of her marriages occurring
+between 1999 and 2002. All occurred either in Westchester County, Long Island, New Jersey or the
+Bronx. She is believed to still be married to four men, and at one time, she was married to eight menat once, prosecutors say. Prosecutors said the immigration scam involved some of her husbands, whofiled for permanent residence status shortly after the marriages. Any divorces happened only after
+such filings were approved. It was unclear whether any of the men will be prosecuted. The case was referred to the Bronx District Attorney Office by Immigration and Customs Enforcement
+and the Department of Homeland Security Investigation Division. Seven of the men are fromso-called"red-flagged" countries, including Egypt, Turkey, Georgia, Pakistan and Mali.Her eighth husband, Rashid Rajput, was deported in 2006 to his native Pakistan after an investigation by the Joint
+Terrorism Task Force.If convicted, Barrientos faces up to four years in prison. Her next court
+appearance is scheduled for May 18.
 """
 
 # summary = summarize_text(input_text)
